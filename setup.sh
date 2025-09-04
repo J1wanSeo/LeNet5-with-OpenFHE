2c25cc13a468584e6bee9c5e61f22c4bdbcaacfc
Jiwan Seo <106726102+J1wanSeo@users.noreply.github.com>
2025-09-04 20:27:42 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -36,14 +36,14 @@ cp environment.yml /tmp/environment.yml
 # ============================
 # 4) Intel HEXL 설치
 # ============================
-git clone https://github.com/intel/hexl.git /opt/intel-hexl
-mkdir -p /opt/intel-hexl/build
-cd /opt/intel-hexl/build
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release ..
-ninja
-ninja install
-ldconfig
-cd ~
+# git clone https://github.com/intel/hexl.git /opt/intel-hexl
+# mkdir -p /opt/intel-hexl/build
+# cd /opt/intel-hexl/build
+# cmake -G Ninja -DCMAKE_BUILD_TYPE=Release ..
+# ninja
+# ninja install
+# ldconfig
+# cd ~
 
 # ============================
 # 5) OpenFHE 빌드 및 설치
@@ -53,8 +53,8 @@ mkdir -p /opt/openfhe/build
 cd /opt/openfhe/build
 cmake -G Ninja \
     -DWITH_OPENMP=ON \
-    -DWITH_INTEL_HEXL=ON \
-    -DINTEL_HEXL_PREBUILT=OFF \
+    # -DWITH_INTEL_HEXL=ON \
+    # -DINTEL_HEXL_PREBUILT=OFF \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=/usr/local \
     ..
@@ -72,11 +72,11 @@ source ~/.bashrc
 # ============================
 # 7) Git 레포지토리 클론
 # ============================
-cd ~
-git clone https://github.com/openfheorg/openfhe-configurator.git
-cd openfhe-configurator
-(echo n; echo y) | ./scripts/configure.sh
-./scripts/build-openfhe-development.sh
+# cd ~
+# git clone https://github.com/openfheorg/openfhe-configurator.git
+# cd openfhe-configurator
+# (echo n; echo y) | ./scripts/configure.sh
+# ./scripts/build-openfhe-development.sh
 
 gnome-terminal -- bash -c "echo 새 터미널에서 실행됨; exec bash"
 exit
